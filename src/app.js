a39d07ae5aff08baa863f945e2ad1d68517eb63c
anastasiiaFro <110201769+anastasiiaFro@users.noreply.github.com>
2022-09-06 09:39:15 +0300

    Connected API, added curent temp, wind, humidity

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
+function displayTemperature(response) {
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
 
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = response.data.main.wind.speed;
+}
+
+let apiKey = "03aebc038ecf36ac244a3fcaa0014291";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
